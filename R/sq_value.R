0796071465d2f1695e3325f0e3fd04cfd57a6067
joran <joran.elias@gmail.com>
2017-09-12 20:31:31 -0600

    docs and ensure singletons with IN class are wrapped in parens

--- R/sq_value.R
+++ R/sq_value.R
@@ -1,9 +1,12 @@
 #' Prepare Value for an SQL Query
 #'
-#' @details \code{NA}s are converted to NULL, other values are coerced to character representation
-#'  with \code{as.character}, and non-numeric and non-NULL values are (single) quoted. Lists
-#'  are wrapped in parentheses and values are separated with commas. The \code{quote} parameter
-#'  is useful for dynamically specifying e.g. columns, or table names.
+#' @details \code{NA}s are converted to NULL, other values are coerced to
+#' character representation with \code{as.character}, and non-numeric and
+#' non-NULL values are (single) quoted. Lists and vectors of length >1 are
+#' wrapped in parentheses and values are separated with commas. You can force
+#' being wrapped in parenthese by using \code{IN}, e.g. \code{sq_value(IN(2))}.
+#' The \code{quote} parameter is useful for dynamically specifying e.g. columns,
+#' or table names.
 #'
 #' @param value A value to be used in an SQL query.
 #' @param quote The character to be used to quote the (non-numeric) value(s).
@@ -16,9 +19,12 @@ sq_value <- function(value, quote = NULL)
   if (inherits(value, "sq_value"))
     return(value)
 
-  if (length(value) > 1) {
-    out <- paste0("(", paste(vapply(value, sq_value, character(1), quote = quote),
-                             collapse = ","), ")")
+  if (inherits(value,"IN") || length(value) > 1) {
+    #Recursive call and collapse with commas
+    out <- paste(vapply(value, sq_value, character(1), quote = quote),
+                 collapse = ",")
+    #Wrap in parens
+    out <- paste0("(", out, ")")
   } else {
     out <- rep("NULL", length(value))
     available <- !is.na(value)
