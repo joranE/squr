76cd3828329cf86a4d25e81c3fe1ae610c7b45f5
Joran Elias <joran.elias@umontana.edu>
2017-09-19 09:57:11 -0600

    make sq_value more strict and specifically tailored to inserting parameter values for SELECT statements; removal of NA to NULL conversion makes it less useful for INSERT statements

--- R/sq_value.R
+++ R/sq_value.R
@@ -1,47 +1,49 @@
 #' Prepare Value for an SQL Query
 #'
-#' @details \code{NA}s are converted to NULL, other values are coerced to
+#' @details \code{NA}s are dropped and at least one element of \code{value}
+#' must be non-\code{NA}. Other values are coerced to their
 #' character representation with \code{as.character}, and non-numeric and
-#' non-NULL values are (single) quoted. Lists and vectors of length >1 are
+#' non-NULL values are (single) quoted. Vectors of length >1 are
 #' wrapped in parentheses and values are separated with commas. You can force
 #' being wrapped in parenthese by using \code{IN}, e.g. \code{sq_value(IN(2))}.
 #' The \code{quote} parameter is useful for dynamically specifying e.g. columns,
 #' or table names.
 #'
-#' @param value A value to be used in an SQL query.
+#' @param value A value or vector of values to be used in an SQL query.
 #' @param quote The character to be used to quote the (non-numeric) value(s).
 #' Can be one of '[', '"', "'" and "none". For brackets, use the opening bracket.
 #' @return A character representation appropriate for SQL queries.
 #'
 #' @export
-sq_value <- function(value, quote = NULL)
-{
+sq_value <- function(value, quote = NULL){
+  #Check that value is suitable
   if (inherits(value, "sq_value"))
     return(value)
+  if (length(value) == 0)
+    stop("'value' must be of non-zero length")
+  if (all(is.na(value)))
+    stop("'value' must have at least one non-NA value")
+  if (!is_valid_vector(value))
+    stop("'value' must be an atomic vector of mode integer, numeric or character")
 
+  #value(s) are either for an IN clause or not
   if (inherits(value,"IN") || length(value) > 1) {
-    #Recursive call and collapse with commas
-    #out <- paste(vapply(value, sq_value, character(1), quote = quote),
-    #             collapse = ",")
+    value <- value[!is.na(value)]
     if (inherits(value,"character")){
       out <- DBI::dbQuoteString(DBI::ANSI(),value)
     }else{
       out <- value
     }
+
     out <- paste(as.character(out),collapse = ",")
-    #Wrap in parens
     out <- paste0("(", out, ")")
   } else {
-    out <- rep("NULL", length(value))
-    available <- !is.na(value)
-
-    if (any(available)) {
-      if (is.numeric(value)) {
-        out[available] <- as.character(value[available])
-      } else {
-        out[available] <- dbi_interpolate(as.character(value[available]), quote)
-      }
-    }
+    out <- dbi_interpolate(value,quote)
   }
-  structure(out, class = c("sq_value", "character"))
+
+  structure(as.character(out), class = c("sq_value", "character"))
+}
+
+is_valid_vector <- function(x){
+  is.atomic(x) && mode(x) %in% c('integer','numeric','character')
 }
